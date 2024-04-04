f43322da8d18d61fbceb471f4a85bf587787c625
yuntasha <jaljayo85@gmail.com>
2024-04-04 22:53:29 +0900

    Init: Initial Commit

---
+++ src/main/java/capstone/replyRecoommend/ReplyRecoommendApplication.java
@@ -0,0 +1,13 @@
+package capstone.replyRecoommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyRecoommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyRecoommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/replyRecoommend/ReplyRecoommendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.replyRecoommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyRecoommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
