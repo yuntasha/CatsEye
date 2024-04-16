372f2b3b439e84ac38f2b66e0e5af72db0e00174
yuntasha <jaljayo85@gmail.com>
2024-04-16 14:44:05 +0900

    Refactor: code refactor

--- src/main/java/capstone/replyRecoommend/gpt/dto/GptRequest.java
+++ src/main/java/capstone/replyRecoommend/gpt/dto/GptRequest.java
@@ -3,7 +3,6 @@ package capstone.replyRecoommend.gpt.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +21,4 @@ public class GptRequest {
         this.maxTokens = maxTokens;
         this.temperature = 0.7f;
     }
-
-    //    private int temperature;
-//    private int maxTokens;
-//    private int topP;
-//    private int frequencyPenalty;
-//    private int presencePenalty;
-
-//    public GPTRequest(String model
-//            , String prompt
-//            , int temperature
-//            , int maxTokens
-//            , int topP
-//            , int frequencyPenalty
-//            , int presencePenalty) {
-//        this.model = model;
-//        this.messages = new ArrayList<>();
-//        this.messages.add(new Message("user",prompt));
-//        this.temperature = temperature;
-//        this.maxTokens = maxTokens;
-//        this.topP=topP;
-//        this.frequencyPenalty=frequencyPenalty;
-//        this.presencePenalty = presencePenalty;
-
-//    }
 }
--- src/main/java/capstone/replyRecoommend/gpt/service/GptServiceImpl.java
+++ src/main/java/capstone/replyRecoommend/gpt/service/GptServiceImpl.java
@@ -1,6 +1,6 @@
 package capstone.replyRecoommend.gpt.service;
 
-import capstone.replyRecoommend.gpt.config.GptConfig;
+//import capstone.replyRecoommend.gpt.config.GptConfig;
 import capstone.replyRecoommend.gpt.config.GptWebClient;
 import capstone.replyRecoommend.gpt.converter.GptConverter;
 import capstone.replyRecoommend.gpt.dto.GptResponse;
@@ -9,7 +9,6 @@ import capstone.replyRecoommend.gpt.dto.ReplyResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @Slf4j
