406c872c7e1d785f96ef5bb0e2a01dbc684bc1e3
jungjoo <wjddis000507@gmail.com>
2024-05-08 18:56:32 +0900

    chat gpt message api 구현

--- src/main/java/capstone/replyRecoommend/gpt/service/GptService.java
+++ src/main/java/capstone/replyRecoommend/gpt/service/GptService.java
@@ -1,8 +1,13 @@
 package capstone.replyRecoommend.gpt.service;
 
-import capstone.replyRecoommend.gpt.dto.ReplyRequestDTO;
-import capstone.replyRecoommend.gpt.dto.ReplyResponseDTO;
+import capstone.replyRecoommend.gpt.dto.ChatDtoReq;
+import capstone.replyRecoommend.gpt.dto.ChatDtoRes;
 
 public interface GptService {
-    ReplyResponseDTO.PostReplyRecommend postReplyRecommend(ReplyRequestDTO.PostReplyRecommend replyRecommend);
+
+    ChatDtoRes.ChatAnwDtoRes initChat(Long userId, ChatDtoReq.initChatReq chatDtoReq);
+
+    ChatDtoRes.ChatAnwDtoRes chat(Long userId, ChatDtoReq.chatReq request);
+
+    void removeChat(Long userId);
 }
