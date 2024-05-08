7065500a3a27fba2fe9bd629df92d42fe01ea8d7
jungjoo <wjddis000507@gmail.com>
2024-05-08 18:56:19 +0900

    chat gpt message api 구현

--- src/main/java/capstone/replyRecoommend/gpt/converter/GptConverter.java
+++ src/main/java/capstone/replyRecoommend/gpt/converter/GptConverter.java
@@ -1,12 +1,10 @@
 package capstone.replyRecoommend.gpt.converter;
 
-import capstone.replyRecoommend.gpt.web.dto.GptResponse;
-import capstone.replyRecoommend.gpt.dto.ReplyResponseDTO;
-
+import capstone.replyRecoommend.gpt.dto.ChatDtoRes;
 public class GptConverter {
-    public static ReplyResponseDTO.PostReplyRecommend toPostReplyRecommend(GptResponse response){
-        return ReplyResponseDTO.PostReplyRecommend.builder()
-                .message(response.getChoices().get(response.getChoices().size()-1).getMessage().getContent())
+    public static ChatDtoRes.ChatAnwDtoRes answer(String content){
+        return ChatDtoRes.ChatAnwDtoRes.builder()
+                .message(content)
                 .build();
     }
 }
