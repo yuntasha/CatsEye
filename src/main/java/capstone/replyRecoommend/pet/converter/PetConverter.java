ee77fd37f610021a6e69d3be934cd223992f6d16
jungjoo <wjddis000507@gmail.com>
2024-05-26 15:10:22 +0900

    Pet - 반려동물 조회 - petId 추가

--- src/main/java/capstone/replyRecoommend/pet/converter/PetConverter.java
+++ src/main/java/capstone/replyRecoommend/pet/converter/PetConverter.java
@@ -10,6 +10,7 @@ public class PetConverter {
     //나의 반려동물 조회 DTo 변환
     public static PetDtoRes.searchPet searchPet(Pet pet){
         return PetDtoRes.searchPet.builder()
+                    .perId(pet.getId())
                     .name(pet.getName())
                     .age(pet.getAge())
                     .species(pet.getSpecies())
