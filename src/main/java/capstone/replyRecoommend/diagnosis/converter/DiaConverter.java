0294ba243b85fa586fbede3e44bf41b7338c6f55
jungjoo <wjddis000507@gmail.com>
2024-05-28 15:58:42 +0900

    diagnosis - 사진 업로드(진단) api 구현

--- src/main/java/capstone/replyRecoommend/diagnosis/converter/DiaConverter.java
+++ src/main/java/capstone/replyRecoommend/diagnosis/converter/DiaConverter.java
@@ -2,6 +2,7 @@ package capstone.replyRecoommend.diagnosis.converter;
 
 import capstone.replyRecoommend.diagnosis.domain.Diagnosis;
 import capstone.replyRecoommend.diagnosis.dto.DiaDtoRes;
+import capstone.replyRecoommend.pet.domain.Pet;
 
 public class DiaConverter {
 
@@ -11,6 +12,15 @@ public class DiaConverter {
                 .name(diagnosis.getPet().getName())
                 .diagnosisResult(diagnosis.getDiagnosisResult())
                 .diagnosisImageUrl(diagnosis.getDiagnosisImageUrl())
+                .build();
+    }
+
+    public static DiaDtoRes.diagnosisRes diagnosisRulDto(Diagnosis diagnosis, Pet pet){
+        return DiaDtoRes.diagnosisRes.builder()
+                .diagnosisId(diagnosis.getId())
+                .petName(pet.getName())
+                .result(diagnosis.getDiagnosisResult())
+                .time(diagnosis.getDiagnosisDay())
                 .build();
     }
 }
---
+++ src/main/java/capstone/replyRecoommend/diagnosis/web/FlaskConfig.java
@@ -0,0 +1,21 @@
+package capstone.replyRecoommend.diagnosis.web;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@Getter
+public class FlaskConfig {
+
+    @Value("web.flask.server.url")
+    private String flaskServerUrl;
+
+    @Bean
+    public FlaskWebClient FlaskWebClient(){
+        return new FlaskWebClient(flaskServerUrl);
+    }
+}
