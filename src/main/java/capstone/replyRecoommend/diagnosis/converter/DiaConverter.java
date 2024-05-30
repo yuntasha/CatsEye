0d7efa131603a46e230ac41f7ddcf639777508cf
jungjoo <wjddis000507@gmail.com>
2024-05-30 13:05:47 +0900

    진단 응답값 - 시간 수정

--- src/main/java/capstone/replyRecoommend/diagnosis/converter/DiaConverter.java
+++ src/main/java/capstone/replyRecoommend/diagnosis/converter/DiaConverter.java
@@ -4,23 +4,26 @@ import capstone.replyRecoommend.diagnosis.domain.Diagnosis;
 import capstone.replyRecoommend.diagnosis.dto.DiaDtoRes;
 import capstone.replyRecoommend.pet.domain.Pet;
 
+import java.time.LocalDate;
+
 public class DiaConverter {
 
     public static DiaDtoRes.searchDiagnosis searchDiaDto(Diagnosis diagnosis){
         return DiaDtoRes.searchDiagnosis.builder()
-                .time(diagnosis.getDiagnosisDay())
+                .day(diagnosis.getDiagnosisDay().toLocalDate())
                 .name(diagnosis.getPet().getName())
                 .diagnosisResult(diagnosis.getDiagnosisResult())
                 .diagnosisImageUrl(diagnosis.getDiagnosisImageUrl())
                 .build();
     }
 
-    public static DiaDtoRes.diagnosisRes diagnosisRulDto(Diagnosis diagnosis, Pet pet){
+    public static DiaDtoRes.diagnosisRes diagnosisRulDto(Diagnosis diagnosis, Pet pet, LocalDate day, String time){
         return DiaDtoRes.diagnosisRes.builder()
                 .diagnosisId(diagnosis.getId())
                 .petName(pet.getName())
                 .result(diagnosis.getDiagnosisResult())
-                .time(diagnosis.getDiagnosisDay())
+                .day(day)
+                .time(time)
                 .build();
     }
 }
